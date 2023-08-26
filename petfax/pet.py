28f3a2c18aed02e0f7ef813a8cdef5ae4f7d17e8
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-08-25 23:33:08 -0400

    added our template and our json data loading method so that it shows in the browser

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,6 +1,9 @@
-from flask import Blueprint
+from flask import ( Blueprint, render_template, json)
 bp = Blueprint('pet', __name__, url_prefix='/pets')
+
+pets = json.load(open('pets.json'))
+print(pets)
 
 @bp.route('/')
 def index():
-    return 'This is the pets index'
+    return render_template('index.html', pets=pets)
