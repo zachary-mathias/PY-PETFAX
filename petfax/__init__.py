1a71eaf786b2cbee7494ca5b88b33a30661fb22a
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-08-26 00:09:16 -0400

    added different views with a informationals about each pet. also added a form page for users to add facts about animals. Not functional at the moment.

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,15 +1,22 @@
-from flask import Flask
+# config                    
+from flask import Flask 
 
-#factory
+# factory
 def create_app():
-  app = Flask(__name__)
+    app = Flask(__name__)
 
-  @app.route('/')
-  def hello():
-    return 'Hello, PetFax!'
-  
-  #register the pet blueprint
-  from . import pet
-  app.register_blueprint(pet.bp)
-  
-  return app
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
+    return app
