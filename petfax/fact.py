9f83f04d426f64c7422212b2b3a8250091bb46ed
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-08-26 23:22:19 -0400

    added the routes for a few display pages dealing with the fact submission form and the userfax page. Everything ok.

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,10 +1,15 @@
-from flask import (Blueprint, render_template)
+from flask import (Blueprint, render_template, request, redirect)
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
-@bp.route('/')
-def index(): 
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        print(request.form)
+        return redirect('/facts ')
+    
     return render_template('facts/index.html')
+
 
 @bp.route('/new')
 def new(): 
